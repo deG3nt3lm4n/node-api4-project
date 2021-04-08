f6f2df87d371a92261804137893c0062943af16b
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-08 19:22:13 -0400

    :smile: added server and api

--- api/server.js
+++ api/server.js
@@ -1,29 +1,26 @@
 const express = require('express')
 const cors = require('cors')
-
+const path = require('path')
 const userRouter = require('./users/users-router')
-// const mw = require('./middleware/middleware')
 
 const server = express()
 
 
 server.use(cors())
 server.use(express.json())
-// server.use(mw.logger)
+server.use(express.static(path.join(__dirname,"../client/build")))
+
 
 server.use('/api/users', userRouter)
 
+
 server.use('/api/', (_,res) => {
-  res.json({data: "api is up"})
+  res.json({
+    data: "api is up",
+    intro: `<h1>Welcome to nothing</h1><p>Enjoy the stay</p><h6>Check out the endpoints</h6>`
+  })
 })
 
-server.get('/', (req,res) => {
-  res.send(`
-    <h1>Welcome to nothing</h1>
-    <p>Enjoy the stay</p>
-    <h6>Check out the endpoints</h6>
-  `)
-})
 
 
 module.exports = server
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
 import './App.css';
 
+function url(path){
+  return process.env.NODE_ENV  === "development"
+  ? `http://localhost:1422${path}` : path
+}
+
+
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+
+    fetch(url("/api/users"))
+      .then(res => res.json())
+      .then(apiData => setData(apiData))
+
+  },[])
+
+
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to nothing</h1>
+        <h2>what is life</h2>
+        <p>Enjoy your stay</p>
+        <h6>Check out the endpoints</h6>
+
+        <h6>The amount of users {data.length}</h6>
       </header>
+
+
+
     </div>
   );
 }
